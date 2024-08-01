303e062e504efca6a35c28e82e89596e72613d61
Jannis Jelten <jannis.jelten@stud.uni-goettingen.de>
2024-08-01 14:39:34 +0200

    Unify rendering of sections on the landing page

--- src/landing_page.rs
+++ src/landing_page.rs
@@ -90,13 +90,7 @@ pub async fn get_landing_page(
         }
 
         #features ."my-64" {
-            ."container mx-auto px-4" {
-                ."ml-4 mb-2 text-xl text-slate-600 font-medium" { "Features" }
-            }
-            ."bg-slate-100 px-4 py-8 md:px-8 lg:rounded-lg lg:container lg:mx-auto" {
-                ."mt-6 mb-12 text-slate-800 text-3xl font-medium text-center leading-10" {
-                    "Get feedback from your audience in real time."
-                }
+            (render_section("Features", "Get feedback from your audience in real time.", html! {
                 ."grid lg:grid-cols-2 gap-16" {
                     ."p-6 bg-white rounded-lg" {
                         ."mb-7 text-2xl text-slate-700 font-medium tracking-tight flex justify-center items-center gap-2" {
@@ -117,20 +111,13 @@ pub async fn get_landing_page(
                         }
                     }
                 }
-            }
+            }))
         }
         script { "initStartPageDemoAnimations()" }
 
         #pricing ."mb-64" {
-            ."container mx-auto px-4" {
-                ."ml-4 mb-2 text-xl text-slate-600 font-medium" { "Plans and Pricing" }
-            }
-
-            ."bg-slate-100 px-4 py-8 md:px-8 lg:rounded-lg lg:container lg:mx-auto" {
-                ."mt-6 mb-12 text-slate-800 text-3xl font-medium text-center leading-10" {
-                    "Get started for free. Upgrade if you need to."
-                }
-                ."flex flex-wrap justify-center gap-20" {
+            (render_section("Plans and Pricing", "Use it for free. Upgrade if you need to.", html! {
+                ."flex max-sm:flex-col justify-center gap-20" {
                     (render_plan(
                         "Free",
                         "Everything you need to get started.",
@@ -160,18 +147,11 @@ pub async fn get_landing_page(
                         ])
                     )
                 }
-            }
+            }))
         }
 
         #mission ."mb-64" {
-            ."container mx-auto px-4" {
-                ."ml-4 mb-2 text-xl text-slate-600 font-medium" { "Mission" }
-            }
-
-            ."bg-slate-100 p-6 lg:rounded-lg lg:container lg:mx-auto" {
-                ."mt-6 mb-12 text-slate-800 text-3xl font-medium text-center leading-10" {
-                    "Simple. Privacy-friendly. Open source."
-                }
+            (render_section("Mission", "Simple. Privacy friendly. Open source.", html! {
                 ."grid md:grid-cols-2 gap-10 text-slate-700" {
                     ."flex flex-col gap-10" {
                         ."p-6 bg-white rounded-lg" {
@@ -180,11 +160,11 @@ pub async fn get_landing_page(
                                 "Privacy friendly"
                             }
                             ."" {
-                                "To be privacy-friendly, the free tier of Svoote is avaible to everyone without creating an account. "
+                                "To be privacy friendly, the free tier of Svoote is avaible to everyone without creating an account. "
                                 "This protects your data and makes operating the website simpler. "
                                 "We don't use cookies to track users, neither the poll-hosters nor the participants. "
                                 "To analyze our website traffic, we use " a ."underline" href="https://plausible.io" { "Plausible" }
-                                ", an EU-based privacy-focused Google-analytics alternative, which does not track users accross websites."
+                                ", an EU-based privacy focused Google-analytics alternative, which does not track users accross websites."
                             }
                         }
 
@@ -213,9 +193,21 @@ pub async fn get_landing_page(
                         }
                     }
                 }
-            }
+            }))
         }
     }, false).into_response());
+}
+
+fn render_section(section_name: &str, heading: &str, content: Markup) -> Markup {
+    return html! {
+        ."container mx-auto mb-2 px-8 text-xl text-slate-600 font-medium" { (section_name) }
+        ."bg-slate-100 px-6 py-12 lg:rounded-lg lg:container lg:mx-auto" {
+            ."mb-12 text-slate-800 text-3xl font-medium text-center leading-10" {
+                (heading)
+            }
+            (content)
+        }
+    };
 }
 
 fn render_plan(
@@ -227,7 +219,7 @@ fn render_plan(
     included: &[&str],
 ) -> Markup {
     return html! {
-        ."max-w-64 p-8 bg-white rounded-lg" {
+        ."w-64 max-sm:w-full p-8 bg-white rounded-lg" {
             ."mb-6 text-2xl text-slate-900 font-medium tracking-tight" { (name) }
             ."mb-10 text-slate-800 tracking-wide leading-normal" { (subtitle) }
             ."mb-3 text-slate-800" {
